bd467e05af21ff13ee154531e964655dcb80bcff
Dale Hancke <dalehancke@hotmail.com>
2023-08-31 21:26:12 +0800

    Update based on missing data

--- Starter_Code/data/SQLChallenge_Queries.sql
+++ Starter_Code/data/SQLChallenge_Queries.sql
@@ -15,7 +15,7 @@ SELECT * FROM employees
 	
 --3. List the manager of each dept along with their dept. number, dept. name, emp. number, last name and first name
 --IS THIS RIGHT?
-SEE DAY 3 example 1.5
+--SEE DAY 3 example 1.5
 SELECT dept_no, dept_name
 FROM departments
 WHERE dept_no IN
@@ -26,32 +26,43 @@ WHERE dept_no IN
 	(
 		SELECT emp_no
 	           FROM employees 
-	(emp_no, last_name, first_name)
+	(emp_no, last_name, first_name);
 
 --4. List dept no for each employee along with their employee no., last name, first name and dept name
 
 SELECT dept_emp.dept_no, dept_emp.emp_no, departments.dept_no, departments.dept_name,
-		employee.emp_no, employee.first_name, employee.last_name,
+		employees.emp_no, employees.first_name, employees.last_name,
 FROM dept_emp
 		LEFT JOIN departments
 		ON departments.dept_no = dept_emp.dept_no
-		LEFT JOIN employee
-		ON employee.emp_no = dept_emp.emp_no
+		LEFT JOIN employees
+		ON employees.emp_no = dept_emp.emp_no;
 	
 	
 -- 5. List first name, last name and sex of each employee whose first name is Hercules and last name begins with a letter B
 
 SELECT * FROM employees
 	(first_name, last_name, , sex) first_name == Hercules
-	AND last_name IS NOT 
+	AND last_name LIKE 'B%';
 	
 --6. List the frequency counts, in decending order, of all of the employee names (ie how many employees share the same name)
 
 SELECT * FROM employees
-	(last_name) decending
+	(last_name) decending = last_name;
 	
 --7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name
+SELECT dept_emp.dept_no, dept_emp.emp_no, departments.dept_no, departments.dept_name,
+		employees.emp_no, employees.first_name, employees.last_name,
+FROM dept_emp
+		LEFT JOIN departments
+		ON departments.dept_no = dept_emp.dept_no == Sales AND Development
+		LEFT JOIN employees
+		ON employees.emp_no = dept_emp.emp_no
 
 --8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name)i
+SELECT COUNT(DISTINCT last_name) 
+FROM employees
+	(last_name) decending;
+
 
 		
